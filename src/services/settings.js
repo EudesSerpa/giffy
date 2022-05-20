9c36c6fb8d81f4afa8c027e14b12eaff1af8eeab
Eudes Serpa <serpagame@gmail.com>
2022-05-20 16:50:36 -0500

    update endpoint to request

--- src/services/settings.js
+++ src/services/settings.js
@@ -2,4 +2,8 @@ export const API_KEY = "xiwf6ZdXNwezA2F4qVqVeKD9Pc7SUK2t";
 
 export const API_URL = "https://api.giphy.com/v1";
 
-export const ENDPOINT = "http://localhost:8080";
+// For Local
+// export const ENDPOINT = "http://localhost:8080";
+
+// For Deno Deploy
+export const ENDPOINT = "https://giffy-api.deno.dev";
